08ebcbd0933adb313020ee773f4f54c7b6952b5b
theoheron <theron@ucsd.edu>
2026-02-24 16:52:04 -0800

    Render Header only in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,48 @@
 export default function Home() {
   return (
     <div className="homePage">
-      {/* NO <Header /> HERE */}
-
       <section className="homeHero">
         <div className="playerFrame">
           <iframe
             src="https://goodymoog.github.io/goodymoog-player5/"
             title="Goodymoog Player"
+            loading="lazy"
+            allowFullScreen
           />
         </div>
       </section>
 
       <section className="homeIntro">
-        <p style={{ color: "white" }}>
-          Born under New Mexico’s vast skies, Goodymoog grew up immersed in the quiet rhythms of the desert. Rooted in patience and exploration, Goodymoog often wanders the desert at dawn, attuning himself to the distant hum of mustard coyotes.
-        </p>
+        <div style={{ textAlign: "center" }}>
+          <p
+            style={{
+              fontFamily:
+                "AVAntiversLatence-Regular, 'Helvetica Neue', Arial, sans-serif",
+              fontWeight: "normal",
+              fontSize: "100%",
+              color: "#F0EFEA",
+            }}
+          >
+            Born under New Mexico’s vast skies, Goodymoog grew up immersed in the
+            quiet rhythms of the desert. Rooted in patience and exploration,
+            Goodymoog often wanders the desert at dawn, attuning himself to the
+            distant hum of mustard coyotes.
+          </p>
+
+          <i>
+            <a
+              href="https://drive.google.com/uc?export=download&id=1GcGpogW8UH4oyk0MVrkclr-C3oyyUKvP"
+              style={{ color: "#cfd0ff", textDecoration: "none" }}
+              download
+            >
+              Download EPK
+            </a>
+          </i>
+
+          <br />
+
+          <a href="https://www.goodymoog.com/learn-more">Read more</a>
+        </div>
       </section>
     </div>
   );
