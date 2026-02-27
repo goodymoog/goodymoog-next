da85af229e3ea3aec139f0444c9bcc995ae8847c
theoheron <theron@ucsd.edu>
2026-02-26 17:14:26 -0800

    Mobile adjust player

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
           />
         </div>
       </section>
-
       {/* <section className="homeIntro">
         <div style={{ textAlign: "center" }}>
           <p
