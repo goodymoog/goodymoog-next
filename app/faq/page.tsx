61f41624120f20b110abfaa9d46e568e4f4c456d
theoheron <theron@ucsd.edu>
2026-04-18 13:48:17 -0700

    Add sparkle effect to FAQ title

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -43,7 +43,7 @@ export default function FAQPage() {
                 backgroundSize: "220% auto",
                 WebkitBackgroundClip: "text",
                 WebkitTextFillColor: "transparent",
-                animation: "sparkleShine 10s linear infinite",
+                animation: "sparkleShine 20s linear infinite",
                 display: "inline-block",
               }}
             >
