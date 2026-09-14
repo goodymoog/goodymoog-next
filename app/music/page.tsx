a8c3ad4bc622dff146595992c28770121fdb2449
theoheron <theron@ucsd.edu>
2026-09-14 17:35:46 -0400

    Add looping music video

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -13,8 +13,14 @@ export default function Home() {
           {/* MAIN */}
           <div className="main-content">
             <h2>2026: Warp Drive</h2>
-            <video className="music-video" controls playsInline>
-              <source src="/videos/warp-drive.mp4" type="video/mp4" />
+            <video
+              className="spinning-cd-video"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/videos/spinning-cd.mp4" type="video/mp4" />
             </video>
             <div className="album-box">
               <p>
