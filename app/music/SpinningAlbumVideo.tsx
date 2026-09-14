86b8410a19ffc4bd41a446de25c9d5fc7f82ad08
theoheron <theron@ucsd.edu>
2026-09-14 17:49:38 -0400

    Fix spinning album video

--- app/music/SpinningAlbumVideo.tsx
+++ app/music/SpinningAlbumVideo.tsx
@@ -1,13 +1,30 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 export default function SpinningAlbumVideo() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+      video.muted = false;
+    }
+  }, []);
+
   return (
     <video
+      ref={videoRef}
       className="spinning-cd-video"
       loop
       playsInline
-      preload="auto"
-      onClick={(e) => e.currentTarget.play()}
+      preload="metadata"
+      onClick={(e) => {
+        e.currentTarget.play();
+      }}
     >
       <source src="/videos/spinning-cd.mp4" type="video/mp4" />
     </video>
